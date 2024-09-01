7bdb036614dc6083a13d0c0dab9f9f0bed785179
Joe Chang <joe.chang.1996@hotmail.com>
2024-09-01 17:59:34 -0600

    Portofolio Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactComponent } from './contact/contact.component';
     AppComponent,
     HeaderComponent,
     InfoComponent, 
+    HomeComponent,
     ContactComponent,
   ],
   imports: [
