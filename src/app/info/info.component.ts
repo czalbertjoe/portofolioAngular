6177960a67132ab4d53cf104757ef036e0fcbf62
Joe Chang <joe.chang.1996@hotmail.com>
2025-01-13 19:58:26 -0600

    portofolio-Joesang-Angular

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,41 +1,42 @@
-import { Component, ElementRef, Renderer2, ViewChild, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
 })
-
-export class InfoComponent implements AfterViewInit {
-  @ViewChild('modal1') modal1!: ElementRef;
-  @ViewChild('modal2') modal2!: ElementRef;
-  @ViewChild('modal3') modal3!: ElementRef;
-
+export class InfoComponent {
   modal1Visible: boolean = false;
   modal2Visible: boolean = false;
   modal3Visible: boolean = false;
 
-  ngAfterViewInit() {
-    // Este método no se está utilizando actualmente, pero es necesario implementarlo
-  }
-
-  showModal(modal: string) {
+  showModal(modal: string): void {
     if (modal === 'modal1') {
-      this.modal1Visible = true;  
-    } else if (modal === 'modal2') {
+      console.log('Abriendo proyectos front espere...'); // Debug
+      this.modal1Visible = true;
+    }
+    if (modal === 'modal2') {
+      console.log('Abriendo proyectos back espere...'); // Debug
       this.modal2Visible = true;
-    } else if (modal === 'modal3') {
+    }
+    if (modal === 'modal3') {
+      console.log('Abriendo otros proyectos espere...'); // Debug
       this.modal3Visible = true;
     }
   }
 
-  hideModal(modal: string) {
+  hideModal(modal: string): void {
     if (modal === 'modal1') {
+      console.log('Cerrando gracias...'); // Debug
       this.modal1Visible = false;
-    } else if (modal === 'modal2') {
+    }
+    if (modal === 'modal2') {
+      console.log('Cerrando gracias...'); // Debug
       this.modal2Visible = false;
-    } else if (modal === 'modal3') {
+    }
+    if (modal === 'modal3') {
+      console.log('Cerrando gracias...'); // Debug
       this.modal3Visible = false;
     }
-  }  
+  }
 }
